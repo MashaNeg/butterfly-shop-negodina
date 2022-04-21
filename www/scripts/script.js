19026d2ba68133e54c15a7ea65152ea69490ddc2
Maria Negodina <mashanegodina@yandex.ru>
2022-04-21 15:56:20 +0300

    Делаю

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,51 @@
-console.log('script work!');
+$(document).ready(function(){
+
+  // Бургер
+  $('.j-burger').on('click', function(){
+
+    $('.j-menu').slideToggle();
+  });
+
+  // Табы в контактах
+  $('.j-tabs-link').on('click', function(event){
+    event.preventDefault();
+
+    $('.j-tabs-link').removeClass('active');
+    $(this).addClass('active');
+
+    let index = $(this).index('.j-tabs-link');
+
+    $('.j-contacts-item').removeClass('active');
+    $('.j-contacts-item').eq(index).addClass('active');
+
+  });
+
+  // Фильтр в наших работах
+  $('.j-catalog-link').on('click', function(event){
+    event.preventDefault();
+
+    let filterData = $(this).data('filter');
+
+    console.log(filterData);
+
+    if (filterData === 'all') {
+      $('.services-item').show();
+      return;
+    }
+
+    $('.services-item').each(function(){
+
+      let typeData = $(this).data('type');
+
+      if (filterData === typeData) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+
+    });
+  });
+
+
+
+});
